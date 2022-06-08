4c83b619729e80af667126fe3e27b9854c0b5570
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-08 12:44:38 +0100

    Update Admin.php

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -20,7 +20,7 @@ class Admin extends Seeder
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '123456', // password
             'remember_token' => Str::random(10),
             'is_admin' => 1
         ]);
